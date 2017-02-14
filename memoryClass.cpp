72fc1e76b9ea98a55fd71775c6e7fc124e0d4791
nusbaoning <544124775@qq.com>
2017-02-14 19:41:15 +0800

    Add files via upload
    finish the basic functions of page cache

--- memoryClass.cpp
+++ memoryClass.cpp
@@ -27,7 +27,7 @@ using namespace std;
 //     }
     
 //     duration = (clock() - start) / (double) CLOCKS_PER_SEC;
-//     cout<<"The time for storing 1,000,000  is "<<duration<<endl;
+//     cout<<"The mytime for storing 1,000,000  is "<<duration<<endl;
 
 //     cout << "The size of the hash map is " << mymap.size() << endl;
 
@@ -46,36 +46,119 @@ using namespace std;
 
 // }
 
-page_descriptor * MemoryDevice::find_get_page(unsigned long inode, unsigned long index) {
-	hash_map<unsigned long, page_descriptor> inodeMap;
-	if (pageCacheDirty.find(inode) != pageCacheDirty.end())
-		inodeMap = pageCacheDirty[inode];
-	else if(pageCacheClean.find(inode) != pageCacheClean.end())
-		inodeMap = pageCacheClean[inode];
-	else
-		return NULL;
-	if (inodeMap.find(index) == inodeMap.end())
+page_descriptor * __find_get_page(doublePageHashMap map, unsigned long inode, unsigned long index) {
+	if (map.find(inode) == map.end())
 	{
-		return NULL; 
+		return NULL;
 	}
-	return &inodeMap[index];
+	pageHashMap pm = map[inode];
+	if (pm.find(index) == pm.end())
+	{
+		return NULL;
+	}
+	return pm[index];
 }
 
+page_descriptor * MemoryDevice::find_get_page(unsigned long inode, unsigned long index) {
+	page_descriptor * result = __find_get_page(pageCacheDirty, inode, index);
+	if (result != NULL)
+	{
+		return result;
+	}
+	result = __find_get_page(pageCacheClean, inode, index);
+	return result;
+}
+
+void __add_to_page_cache(doublePageHashMap * map, 
+	page_descriptor * p, unsigned long inode, unsigned long index) {
+	if (map->find(inode) == map->end())
+	{
+		pageHashMap *map[inode];
+	}
+	(*map)[inode][index] = p;
+}
 
 page_descriptor * MemoryDevice::add_to_page_cache(unsigned long inode, unsigned long index, 
-	bool dirty, unsigned long time) {
+	bool dirty, unsigned long mytime) {
+	// cout << "test\t" << inode << "\t" << index << "\t" << dirty << "\t" << mytime << endl;
 	if (allocatedPageNumber < size)
 	{
 		allocatedPageNumber++;
+
+		page_descriptor * p = new page_descriptor();
+		p->dirty = dirty;
+		p->mytime = mytime;
+		p->inode = inode;
+		p->index = index;
+		if (dirty)
+		{
+			__add_to_page_cache(&pageCacheDirty, p, inode, index);
+		}
+		else
+			__add_to_page_cache(&pageCacheClean, p, inode, index);
 	}
-	return NULL;
+	else {
+		shrink_cache();
+		add_to_page_cache(inode, index, dirty, mytime);
+	}
+}
+
+
+void MemoryDevice::remove_from_page_cache(page_descriptor * p) {
+	if (p == NULL)
+	{
+		cout << "p == null" << endl;
+		return;
+	}
+	// cout << p->inode << "\t" << p->index << "\t" << p->dirty << endl;
+	doublePageHashMap * map;
+	if (p->dirty)
+	{
+		map = &pageCacheDirty;
+		// pageCacheDirty[p->inode].erase(p->index);
+	}
+	else {
+		map = &pageCacheClean;
+		// pageCacheClean[p->inode].erase(p->index);
+	}
+
+	(*map)[p->inode].erase(p->index);
+	if ((*map)[p->inode].size() == 0)
+	{
+		map->erase(p->inode);
+	}
+
+	allocatedPageNumber--;
+	cout << "erase finished" << endl;
+	delete p;
+}
+
+void MemoryDevice::set_page_dirty(page_descriptor * page, bool dirty, unsigned long mytime) {
+	unsigned long inode = page->inode;
+	unsigned long index = page->index;
+	remove_from_page_cache(page);	
+	add_to_page_cache(inode, index, dirty, mytime); 
 }
 
 int main() {
 	MemoryDevice m(500);
 	m.show_page_cache_statics();
 	m.add_to_page_cache(0, 0, true, 0);
+	m.add_to_page_cache(0, 1, true, 0);
+	m.add_to_page_cache(2, 0, true, 0);
+	m.add_to_page_cache(2, 1, false, 0);
 	m.show_page_cache_statics();
+	m.show_page_cache_pages();
+	cout << "remove 1" << endl;
+	m.remove_from_page_cache(m.find_get_page(0,0));
+	cout << "remove 2" << endl;
+	m.remove_from_page_cache(m.find_get_page(0,1));
+	cout << "remove 3" << endl;
+	m.remove_from_page_cache(m.find_get_page(2,0));
+	cout << "remove 4" << endl;
+	m.remove_from_page_cache(m.find_get_page(2,1));
+	m.show_page_cache_statics();
+	m.show_page_cache_pages();
 }
 
 
--- memoryClass.h
+++ memoryClass.h
@@ -5,15 +5,18 @@ using namespace std;
 
 typedef struct {
 	bool dirty;
-	unsigned long time;
-	// unsigned long index;
-	// unsigned long inode;
+	unsigned long mytime;
+	unsigned long index;
+	unsigned long inode;
 }page_descriptor;
+
+typedef hash_map<unsigned long, page_descriptor*> pageHashMap;
+typedef hash_map<unsigned long, pageHashMap> doublePageHashMap; 
 
 class MemoryDevice {
 private:
-	hash_map<unsigned long, hash_map<unsigned long, page_descriptor> > pageCacheDirty;
-	hash_map<unsigned long, hash_map<unsigned long, page_descriptor> > pageCacheClean;
+	doublePageHashMap pageCacheDirty;
+	doublePageHashMap pageCacheClean;
 	unsigned int size;
 	unsigned int allocatedPageNumber;
 
@@ -28,22 +31,65 @@ public:
 	}
 
 	/*
+		Note : Only call for small functions
+		output: All the pages with their information, each page one line
+	*/
+	void show_page_cache_pages() {
+		cout << "Dirty" << endl;
+		for (doublePageHashMap::iterator iter = pageCacheDirty.begin(); 
+                                        iter != pageCacheDirty.end(); ++iter)
+		{
+			cout << "Key" << iter->first << endl;
+    		pageHashMap map = iter->second;
+    		for (pageHashMap::iterator iter2 = map.begin(); iter2 != map.end(); ++iter2)
+    		{
+    			cout << iter -> first << "\t" << iter2->first << "\t" << iter2->second->mytime << endl;
+    		}
+		}
+
+		cout << "Clean" << endl;
+		for (doublePageHashMap::iterator iter = pageCacheClean.begin(); 
+                                        iter != pageCacheDirty.end(); ++iter)
+		{
+			cout << "Key" << iter->first << endl;
+    		pageHashMap map = iter->second;
+    		for (pageHashMap::iterator iter2 = map.begin(); iter2 != map.end(); ++iter2)
+    		{
+    			cout << iter -> first << "\t" << iter2->first << "\t" << iter2->second->mytime << endl;
+    		}
+		}
+	}
+
+	/*
 		function : search the page in page cache
 		return the page address if found, otherwise return NULL
 	*/
 	page_descriptor * find_get_page(unsigned long inode, unsigned long index);
 
 	/*
-		input : the inode and index of the page, the dirty sign and the time of the request
+		input : the inode and index of the page, the dirty sign and the mytime of the request
 		function : add the page into page cache, change the page number
+		note : only add, no other checks, do not remove the existed record if it is in another map
 	*/
 	page_descriptor * add_to_page_cache(unsigned long inode, unsigned long index, 
-	bool dirty, unsigned long time);
+	bool dirty, unsigned long mytime);
 
+	/*
+		input : the pointer of the page, basically come from find_get_page
+		function : free the space of the page_descriptor and delete the item from hash_map
+	*/
 	void remove_from_page_cache(page_descriptor * p); 
 
-	void set_page_dirty(page_descriptor * page, bool dirty, unsigned long time);
+	/*
+		function : set the page as dirty with time
+	*/
+	void set_page_dirty(page_descriptor * page, bool dirty, unsigned long mytime);
+
+	virtual void shrink_cache();
 
 };
 
+void MemoryDevice::shrink_cache() {
+	return;
+}
 
