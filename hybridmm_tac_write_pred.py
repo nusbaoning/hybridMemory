b28923511a689ec1bcc2e23158ce855b357963db
nusbaoning <544124775@qq.com>
2017-04-25 21:45:11 +0800

    Add files via upload
    基本完成了程序功能和测试。
    目前可以跑通filebench 200MB的文件，但是结果不好，可能因为源数据质量不好。

--- hybridmm_tac_write_pred.py
+++ hybridmm_tac_write_pred.py
@@ -3,14 +3,22 @@ import codecs
 import time
 import sys
 from operator import itemgetter
+from enum import Enum
+from random import randint
+
 
 HEADLINENUMBER = 11
 MINLENGH = 10
+READ_FUNCTION_NAME = "generic_file_read_iter"
+STAGEMAX = 3
 
 page = {}  
 writePeriod = 100000
 writeReq = 0
+totalWriteReq = 0
 sortedList = []
+current = 0
+maxCount = 0
 class PartitionType(Enum):
 	time = 0
 	nrreq = 1
@@ -18,7 +26,7 @@ class PartitionType(Enum):
 
 
 def load_file(filename, parttType):  
-    global page, writeReq
+    global page, writeReq, current, maxCount, totalWriteReq
     s = time.time()
     fin = codecs.open(filename, 'r', encoding='utf-8', errors='ignore')          
     lines = fin.readlines()
@@ -27,6 +35,8 @@ def load_file(filename, parttType):
     s = e
     lineNum = len(lines)
     stage = 0
+    # sign = False
+    i = 0
     for line in lines:
         i=i+1
         # print(i, readreq, writeReq)
@@ -66,16 +76,20 @@ def load_file(filename, parttType):
         j+=1
         count = int(items[j])
         j+=2
+        if count > maxCount:
+        	maxCount = count
         filename = items[j]            
         current = timestamp
-
+        # if not sign:
+        # 	print(task, pid, timestamp, inode, pos, count, filename)
+        # 	sign = True
 #           finish parse
         
         
         index_0 = pos >> 12
         index_1 = (pos + count - 1) >> 12
         writeReq += index_1 + 1 - index_0
-
+        totalWriteReq += index_1 + 1 - index_0
         for index in range(index_0, index_1 + 1):     
             if not (inode, index) in page.keys():                
                 page[(inode, index)] = 1
@@ -83,7 +97,7 @@ def load_file(filename, parttType):
                 written = page[(inode, index)]
                 page[(inode, index)] = written + 1              
         
-        if finishPeriod(parttType, timestamp):
+        if finishPeriod(parttType, timestamp, stage):
         	stage = store_page(stage)
         	
         
@@ -95,25 +109,49 @@ def load_file(filename, parttType):
     fin.close()
 
 
-def finishPeriod(parttType, timestamp):
+def finishPeriod(parttType, timestamp, stage):
 	global writeReq
-
+	periodCoef = [1, 3, 5, 10]
 	if parttType == PartitionType.time:
 		return False
-
-	if writeReq >= writePeriod:
+	# req mode
+	writeStandard = writePeriod * periodCoef[stage]
+	if writeReq >= writeStandard:
+		if stage == 0 or stage == STAGEMAX:
+			writeReq = 0		
 		return True
 	else:
 		return False
 
 def get_sorted_list():
-	l = (list)page.items()
+	l = []
+	for k,v in page.items():
+		l.append((k,v)) 	
 	l.sort(key=itemgetter(1), reverse=True)
 	return l
 		
 
-def compare_two_list(newList):
-	percent = 
+def compare_two_list(newList, d):
+	percent = [0.1, 0.2, 0.5]
+	r = []
+	for item in percent:
+		num = int(item*len(sortedList))
+		# print("num = ", num)
+		_, req = newList[num-1]
+		print(req)
+		r.append(compare_two_list_core(sortedList[0:num], req, d))
+	return r
+
+def compare_two_list_core(l1, req, d):
+	# print(l1)
+	i = 0
+	for item in l1:
+		page, _ = item
+		if page in d:
+			reqPage = d[page]
+			if reqPage >= req:
+				i += 1
+	return round(i/len(l1)*100, 2)
 
 
 def store_page(stage):
@@ -121,17 +159,33 @@ def store_page(stage):
 	# stage can be 0,1,2,3
 	# 0 is for observation
 	# 1,2,3 are for comparing
-	stageMax = 3
+	
 	if stage == 0:
 		sortedList = get_sorted_list()
 		page = {}
 		return stage+1
 
 	newSortedList = get_sorted_list()
-	compare_two_list(newSortedList)
+	r = compare_two_list(newSortedList, page)
+	record_result(stage, r)
 
-	if stage == stageMax:
+	if stage == STAGEMAX:
 		page = {}
 		return 0
 	else:
 		return stage+1
+
+def record_result(stage, r):
+	print(stage, r)
+	print(stage, r, file=f)
+
+f = open("result.log", "a")
+
+# simple example
+# page = {(1,1):1, (1,2):2, (1,3):8, (1,4):9,
+# (1,5):3, (1,7):2, (1,6):2, (1,8):3, (1,9):20, (1,10):42}
+fn = "nd_filebench5"
+print(["*"]*20, file=f)
+load_file(fn, PartitionType.nrreq)
+f.close()
+print(totalWriteReq)
